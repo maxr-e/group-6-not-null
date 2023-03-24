d1780d65e8a1fda64c4b6b8f6acf677dbf21ec87
J <09.chen@gmail.com>
2023-03-23 21:34:06 -0700

    login html + js

---
+++ public/js/login.js
@@ -0,0 +1,48 @@
+var loginbutton = document.querySelector("#loginbutton");
+var signupbutton = document.querySelector("#signupbutton");
+
+const login = async (event) => {
+    event.preventDefault();
+    const username = document.querySelector("#username-login").value.trim();
+    const password = document.querySelector("#password-login").value.trim();
+
+    if (username && password) {
+        const response = await fetch('/login', {
+          method: 'POST',
+          body: JSON.stringify({ username, password }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+    if (response.ok) {
+            document.location.replace('/');
+          } else {
+            alert('Failed to log in.');
+          }
+}}
+
+const signup = async (event) => {
+    event.preventDefault();
+    const name = document.querySelector('#name-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    const age = document.querySelector('#age-signup').value.trim();
+  
+    if (name && username && email && password && age) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ name, username, email, password, age}),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to sign up.');
+      }
+    }
+  };
+  
+
+loginbutton.addEventListener("click", login);
+
+signupbutton.addEventListener("click", signup);
---
+++ public/js/logout.js
@@ -0,0 +1,17 @@
+var logoutbutton = document.querySelector("#logout");
+
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log out.');
+    }
+  };
+  
+  logoutbutton.querySelector('#logout').addEventListener('click', logout);
+  

fd5538419f95f29397d129548027b248489c0e9d
Jordan Pace <jordanpace95@gmail.com>
2023-03-24 21:14:49 -0600

    adds routes and api routes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// create a post
+router.post('/', async (req, res) => {
+    try {
+      const newPost = await Post.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newPost);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  router.delete('/:id', async (req, res) => {
+    try {
+      const postData = await Post.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;

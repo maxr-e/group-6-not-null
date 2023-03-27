eeaa16931e6e2aa6777b832bb6e129d29bb2ca57
J <09.chen@gmail.com>
2023-03-26 20:28:44 -0700

    signup

--- public/js/login.js
+++ public/js/login.js
@@ -21,16 +21,15 @@ const login = async (event) => {
 
 const signup = async (event) => {
     event.preventDefault();
-    const name = document.querySelector('#name-signup').value.trim();
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
     const age = document.querySelector('#age-signup').value.trim();
   
-    if (name && username && email && password && age) {
+    if (username && email && password && age) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, username, email, password, age}),
+        body: JSON.stringify({username, email, password, age}),
         headers: { 'Content-Type': 'application/json' },
       });
   
